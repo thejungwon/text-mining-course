a455a3525cc6e47a56023488924fb217bc76a885
thejungwon <wsbtgt@gmail.com>
2019-11-08 10:58:28 +0900

    update

--- practice/etc/runfile/sample/hello_world.py
+++ practice/etc/runfile/sample/hello_world.py
@@ -1,2 +1,7 @@
+ID = raw_input("Please type your ID: ")
+PW = raw_input("Please type your PW: ")
+
 with open("hello_world.txt", "w") as f:
-    f.write("hello world")
+    f.write("ID:"+ID+"\n")
+    f.write("PW:"+PW+"\n")
+    f.write("hello world\n")
